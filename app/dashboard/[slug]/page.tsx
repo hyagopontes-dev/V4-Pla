bb6dadeb3755beed222ad86a0802ea1685428c14
unknown <hyago.pontes@v4company.com>
2026-04-22 13:59:29 -0300

    deix

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,4 +1,5 @@
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 import { createClient } from '@/lib/supabase'
 import DeliverableView from '@/components/client/DeliverableView'
